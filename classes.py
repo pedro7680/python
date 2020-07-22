6b87acc8d6ba937bbb1d6aff06d6d2603daf56f8
pedro7680 <ped1704@gmail.com>
2020-07-22 17:18:27 +0100

    flight class added

--- classes.py
+++ classes.py
@@ -10,3 +10,30 @@ print(p.x)
 print(p.y)
 # print the co-ords in formatted string. 
 print(f"Co-ordinates of the point are x:{p.x} and y:{p.y}.")
+
+# more classes
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True    
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+       
+
+flight = Flight(3)
+
+people = ["Harry", "Peter", "Jillian", "Barry"]
+# loop and add these people to the flight
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"no seats for {person}")
